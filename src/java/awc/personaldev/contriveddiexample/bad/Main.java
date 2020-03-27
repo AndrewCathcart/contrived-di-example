0e82be65cba05f9129f313cbabbae0ab32b8ec37
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-03-27 00:41:48 +0000

    Explanation of bad design

--- src/java/awc/personaldev/contriveddiexample/bad/Main.java
+++ src/java/awc/personaldev/contriveddiexample/bad/Main.java
@@ -1,5 +1,12 @@
 package java.awc.personaldev.contriveddiexample.bad;
 
+/*
+Application class is responsible for initializing the FacebookMessengerService and then using it.
+This results in a hardcoded dependency - if we want to switch to some other messaging service in the future it will require code changes inside the Application class.
+This makes our application hard to extend - if the FacebookMessengerService is used in multiple classes then that furthers the difficulty.
+If we wanted to extend this application to provide additional messaging features, such as direct messages on Twitter, we'd need to write another application.
+Testing this application will be difficult as it directly creates an instance of FacebookMessengerService, no way we can mock these private services.
+ */
 public class Main {
     public static void main(String[] args) {
         Application app = new Application();
